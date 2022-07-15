db1d530516cf2930b1e4a36c03b5b04ebaacbc14
sunny0402 <sunnycodes0402@gmail.com>
2022-07-15 07:14:02 -0400

    Connect to MongoDB.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -8,7 +9,12 @@ const errorHandler = require("./middleware/errorHandler");
 const verifyJWT = require("./middleware/verifyJWT");
 const cookieParser = require("cookie-parser");
 const credentials = require("./middleware/credentials");
+//db set-up
+const mongoose = require("mongoose");
+const connectDB = require("./config/dbConn");
 const PORT = process.env.PORT || 3500;
+
+connectDB();
 
 // custom middleware logger
 app.use(logger);
@@ -57,4 +63,8 @@ app.all("*", (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//Listen for requests only if succesfully connected to database.
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB.");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
